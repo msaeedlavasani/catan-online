6de229350cc6b04172f0a41e93e5bbf07c5f3da6
msaeedlavasani <155816779+msaeedlavasani@users.noreply.github.com>
2026-08-02 15:44:35 +0330

    Update SERVER_URL to use window.location.origin

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:4000";
+const SERVER_URL =
+  import.meta.env.VITE_SERVER_URL || window.location.origin;
 
 export const socket = io(SERVER_URL, {
   autoConnect: true,
